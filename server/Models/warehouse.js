baad5ed18d661aba3d11a9fb38eac0360094175a
Moaaz Mostafa <moaazmostafa7474@gmail.com>
2023-04-10 01:58:56 +0200

    add warehouse and auth modules

---
+++ server/Models/warehouse.js
@@ -0,0 +1,60 @@
+const conn = require('../db/connection');
+const util  = require('util');
+const query = util.promisify(conn.query).bind(conn); // transform query mysql --> promise to use [await/async]
+//const supervisor = require('../Models/supervisor');
+
+class Warehouse{
+
+    constructor(){}
+
+    async createWarehouse(warehouse){
+        await query("INSERT into warehouse SET ? ",warehouse);
+        return true;
+    }
+
+    async deleteWarehouse(id){
+        await query("DELETE from warehouse WHERE warehouse.id = ?" , [id]);
+    }
+
+    async upadateWarehouse(id , warehouse){
+        await query("UPDATE warehouse SET ? WHERE id = ?" , [warehouse , id])
+    }
+    
+
+    async getSpecificWarehouse(id){
+
+        const warehouses =  await this.getWarehouses();
+        console.log(warehouses);
+        for (let i = 0; i < warehouses.length; i++) {
+            const jsonWarehouse = warehouses[i];
+            for(let key in jsonWarehouse){
+                if(jsonWarehouse[key] == id)return jsonWarehouse;
+            }
+        }
+        return "Error";
+    }
+
+    async getWarehouses(){
+
+        // USED LEFT JOIN TO GET ALL WAREHOUSES THAT CONTAIN PRODUCTS OR NOT
+        const warehousesRaw = await query("SELECT warehouse.id , warehouse.name ,warehouse.location,warehouse.state,user.id as supervisorId,user.email,SUM(product.stock) as totalStockQuantity FROM warehouse JOIN user on warehouse.supervisorId = user.id LEFT JOIN product on product.warehouseId = warehouse.id  GROUP BY warehouse.id,warehouse.name");
+        const warehouses = JSON.stringify(warehousesRaw);
+        const data = JSON.parse(warehouses);
+        return data;
+    }
+
+    async getWarehouseByName(name){
+
+        const warehouseRaw = await query("SELECT * FROM `warehouse` WHERE warehouse.name = ?",name);
+        return warehouseRaw;
+    }
+
+    async getWarehouseById(id){
+        const warehouseRaw = await query("SELECT * FROM `warehouse` WHERE warehouse.id = ?",id);
+        return warehouseRaw;
+    }
+
+}
+
+
+module.exports = Warehouse;
---
+++ server/middleware/admin.js
@@ -0,0 +1,22 @@
+const conn = require('../db/connection');
+
+const authorized = (req,res,next) => {
+    const accessToken = req.headers.token;
+    const query = ' SELECT * FROM user WHERE token = ? ';
+            conn.query(query, [accessToken], (error, result) => {
+                if(result[0] && result[0].type=="admin"){
+                    res.locals.user = result[0] ;
+                    next();
+                }
+                else {
+                    res.status(403).json({ msg: "you are not authorized to access this" });
+                }
+                
+            })
+    
+
+}
+module.exports = authorized;
+
+
+///const authorized = require("../middleware/auth") 
